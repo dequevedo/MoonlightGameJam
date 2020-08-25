f89fa5c20b5d05520e958e65874ada36e4e9495d
dequevedo <danielelvisquevedo@gmail.com>
2020-08-25 19:29:25 -0300

    cleanup

--- Assets/Scripts/MonsterSpawner.cs
+++ Assets/Scripts/MonsterSpawner.cs
@@ -6,17 +6,29 @@ namespace Pathfinding {
 public class MonsterSpawner : MonoBehaviour
     {
         public GameObject monster;
+        public bool enableMonsterPatrol = true;
+        private Vector2 originalPosition;
+        public float randomPositionRadius = 1;
 
         void Start()
         {
+            originalPosition = transform.position;
+            if(enableMonsterPatrol) StartCoroutine("MoveSpawnRandomly");
+
             GameObject monsterInstance = Instantiate(monster, transform.position, Quaternion.identity);
             EnemyNavigation enemyNavigation = monsterInstance.GetComponent<EnemyNavigation>();
             enemyNavigation.spawnObject = this.gameObject;
             enemyNavigation.playerObject = GameObject.FindGameObjectWithTag("Player");
-            
-            /*monsterInstance.TryGetComponent<AIDestinationSetter>(out AIDestinationSetter component);
-            GameObject monsterTarget = Instantiate(new GameObject(), transform.position, Quaternion.identity);
-            component.target = monsterTarget.transform;*/
+        }
+
+        private IEnumerator MoveSpawnRandomly()
+        {
+            yield return new WaitForSeconds(Random.Range(1f,3f));
+            transform.position = new Vector2(
+                originalPosition.x + Random.Range(-randomPositionRadius, randomPositionRadius),
+                originalPosition.y + Random.Range(-randomPositionRadius, randomPositionRadius)
+                );
+            StartCoroutine("MoveSpawnRandomly");
         }
     }
 }
---
+++ Assets/Scripts/MoveTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour
+{
+    public float speed = 10;
+    void Update()
+    {
+        Vector3 movement = new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical"),
+            0
+        ).normalized * Time.deltaTime * speed;
+        
+        transform.Translate(movement);
+    }
+}
