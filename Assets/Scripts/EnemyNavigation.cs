6dcb2d5903ea8fc52b8af5b3685285a1df15fe31
dequevedo <danielelvisquevedo@gmail.com>
2020-08-25 01:44:20 -0300

    AI added

--- Assets/Scripts/EnemyNavigation.cs
+++ Assets/Scripts/EnemyNavigation.cs
@@ -1,64 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+using System.Linq;
 
-public class EnemyNavigation : MonoBehaviour
-{
-    public GameObject spawnObject;
-    public GameObject playerObject;
-    public float playerDetectionRange;
-    public float stoppingDistance;
-
-    [Header("Prefabs")]
-
-    public GameObject projectile;
-
-    [Header("Debugging")]
-    public float distanceFromPlayer = 999;
-    public bool playerInSight = false;
-    private NavMeshAgent agent;
-    private RaycastHit hit;
-    private LineRenderer lineRenderer;
-
-    void Start()
+namespace Pathfinding {
+    public class EnemyNavigation : MonoBehaviour
     {
-        lineRenderer = GetComponent<LineRenderer>();
-        agent = GetComponent<NavMeshAgent>();
-        agent.stoppingDistance = stoppingDistance;
-    }
+        public GameObject spawnObject;
+        public GameObject playerObject;
+        public float playerDetectionRange;
+        public float stoppingDistance;
 
-    void Update()
-    {
-        isPlayerInSight();
-        updateLineRenderer();
+        [Header("Prefabs")]
 
-        distanceFromPlayer = Vector3.Distance(transform.position, playerObject.transform.position);
+        public GameObject projectile;
 
-        if(distanceFromPlayer <= playerDetectionRange){
-            agent.SetDestination(playerObject.transform.position);
-            //Instantiate(projectile, transform.position, transform.rotation);
-        } else {
-            agent.SetDestination(spawnObject.transform.position);
+        [Header("Debugging")]
+        public float distanceFromPlayer = 999;
+        public float raycastHitDistance = 999;
+        public bool playerInSight = false;
+        public bool playerInRange = false;
+        private RaycastHit2D hit;
+        private LineRenderer lineRenderer;
+        public AIDestinationSetter destinationSetter;
+
+        void Start()
+        {
+            lineRenderer = GetComponent<LineRenderer>();
         }
-    }
 
-    void isPlayerInSight(){
-        if (Physics.Raycast(transform.position, playerObject.transform.position - transform.position, out hit, playerDetectionRange) 
-        && hit.transform.tag == "Player"){
-            playerInSight =  true;
-        } else {
-            playerInSight =  false;
+        void Update()
+        {
+            isPlayerInSight();
+            isPlayerInRange();
+            updateLineRenderer();
+
+            distanceFromPlayer = Vector3.Distance(transform.position, playerObject.transform.position);
+
+            if(playerInSight && playerInRange){
+                destinationSetter.target = playerObject.transform;
+                GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity);
+                bullet.GetComponent<Projectile>().setMoveDirection(playerObject.transform.position - transform.position);
+            } else {
+                destinationSetter.target = spawnObject.transform;
+            }
         }
-    }
 
-    void updateLineRenderer(){
-        if(playerInSight){
-            lineRenderer.enabled = true;
-            lineRenderer.SetPosition(0, transform.position);
-            lineRenderer.SetPosition(1, playerObject.transform.position);
-        }else{
-            lineRenderer.enabled = false;
+        void isPlayerInSight(){
+            //Implementar raycast com layermask
+            hit = Physics2D.Raycast(transform.position, playerObject.transform.position - transform.position);
+            if (hit.collider != null)
+            {
+                playerInSight = (hit.transform.tag == "Player");
+                raycastHitDistance = Mathf.Abs(hit.point.y - transform.position.y);
+            }
+        }
+
+        void isPlayerInRange(){
+            distanceFromPlayer = Vector3.Distance(transform.position, playerObject.transform.position);
+            playerInRange = (distanceFromPlayer <= playerDetectionRange);
+        }
+
+        void updateLineRenderer(){
+            if(playerInSight){
+                lineRenderer.enabled = true;
+                lineRenderer.SetPosition(0, transform.position);
+                lineRenderer.SetPosition(1, playerObject.transform.position);
+            }else{
+                lineRenderer.enabled = false;
+            }
         }
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float speed;
+    public GameObject player;
+    private Vector2 moveDirection;
+
+    void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     void Update()
     {
-        transform.Translate(new Vector3(0,0,speed * Time.deltaTime));
+        transform.Translate(new Vector2(
+            moveDirection.x * Time.deltaTime * speed, 
+            moveDirection.y * Time.deltaTime * speed));
+    }
+
+    public void setMoveDirection(Vector2 direction){
+        moveDirection = direction;
     }
 }
