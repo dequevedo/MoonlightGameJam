6dcfa3182a8340f17e61c8edf90ee91a7b27f004
dequevedo <danielelvisquevedo@gmail.com>
2020-08-31 02:00:34 -0300

    Sounds

---
+++ Assets/Scripts/RandomMusicSetter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMusicSetter : MonoBehaviour
+{
+    public List<AudioClip> musicList;
+    public AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = musicList[Random.Range(0,musicList.Count)];
+        audioSource.Play();
+    }
+}
